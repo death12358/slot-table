6e77592e3760803e3ff4c773e98e4b18ddcb0618
death12358 <l10714056@gmail.com>
2024-09-27 11:21:58 +0800

    ymal tag修正

--- utils/rtp/limitconfig.go
+++ utils/rtp/limitconfig.go
@@ -7,22 +7,19 @@ import (
 )
 
 type LimitConfig struct {
-	BaseBet                              int64 `yaml:"base_bet"`                            // 基礎投注額
-	SysRTPLimitEnabled                   bool  `yaml:"sys_rtp_limit_enabled"`               // 系統RTP上限功能
-	SysRTPLimit                          int32 `yaml:"sys_rtp_limit"`                       // 系統RTP上限（萬分比)
-	MonthlySysLossLimitEnabled           bool  `yaml:"monthly_sys_loss_limit_enabled"`      // 當月系統虧損上限功能
-	MonthlySysLossLimit                  int64 `yaml:"monthly_sys_loss_limit"`              // 當月系統虧損上限（分）
-	DailySysLossLimitEnabled             bool  `yaml:"daily_sys_loss_limit_enabled"`        // 當日系統虧損上限功能
-	DailySysLossLimit                    int64 `yaml:"daily_sys_loss_limit"`                // 當日系統虧損上限（分）
-	DailyPlayerProfitLimitEnabled        bool  `yaml:"daily_player_profit_limit_enabled"`   // 當日個人盈利上限功能
-	DailyPlayerProfitLimit               int64 `yaml:"daily_player_profit_limit"`           // 當日個人盈利上限（分）
-	MonthlyPlayerProfitLimitEnabled      bool  `yaml:"monthly_player_profit_limit_enabled"` // 當月個人盈利上限功能
-	MonthlyPlayerProfitLimit             int64 `yaml:"monthly_player_profit_limit"`         // 當月個人盈利上限（分）
-	MonthlyPlayerProfitLowerLimitEnabled bool  `yaml:"monthly_player_profit_limit_enabled"` // 當月個人盈利上限功能
-	MonthlyPlayerRTPLowerLimit           int64 `yaml:"monthly_player_rtp_lower_limit"`      // 當月個人RTP下限（分）
-
-	// RakeEnabled     bool `yaml:"rake_enabled"`     // 抽水功能
-	// CashbackEnabled bool `yaml:"cashback_enabled"` // 補水功能
+	BaseBet                              int64 `yaml:"base_bet"`                                  // 基礎投注額
+	SysRTPLimitEnabled                   bool  `yaml:"sys_rtp_limit_enabled"`                     // 系統RTP上限功能
+	SysRTPLimit                          int32 `yaml:"sys_rtp_limit"`                             // 系統RTP上限（萬分比)
+	MonthlySysLossLimitEnabled           bool  `yaml:"monthly_sys_loss_limit_enabled"`            // 當月系統虧損上限功能
+	MonthlySysLossLimit                  int64 `yaml:"monthly_sys_loss_limit"`                    // 當月系統虧損上限（分）
+	DailySysLossLimitEnabled             bool  `yaml:"daily_sys_loss_limit_enabled"`              // 當日系統虧損上限功能
+	DailySysLossLimit                    int64 `yaml:"daily_sys_loss_limit"`                      // 當日系統虧損上限（分）
+	DailyPlayerProfitLimitEnabled        bool  `yaml:"daily_player_profit_limit_enabled"`         // 當日個人盈利上限功能
+	DailyPlayerProfitLimit               int64 `yaml:"daily_player_profit_limit"`                 // 當日個人盈利上限（分）
+	MonthlyPlayerProfitLimitEnabled      bool  `yaml:"monthly_player_profit_limit_enabled"`       // 當月個人盈利上限功能
+	MonthlyPlayerProfitLimit             int64 `yaml:"monthly_player_profit_limit"`               // 當月個人盈利上限（分）
+	MonthlyPlayerProfitLowerLimitEnabled bool  `yaml:"monthly_player_profit_lower_limit_enabled"` // 當月個人盈利上限功能
+	MonthlyPlayerRTPLowerLimit           int64 `yaml:"monthly_player_rtp_lower_limit"`            // 當月個人RTP下限（分）
 }
 
 // 限制配置數值驗證
